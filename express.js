6f4c54fc119fffae85121b8def00c10abac852a9
walk8243 <walk8243@gmail.com>
2018-08-06 15:46:19 +0900

    expressの設定ファイル内でconfigファイルの読み込み

--- express.js
+++ express.js
@@ -1,5 +1,15 @@
 var Express = require('express');
+var configFile = require('config');
 
+
+function config(key = NULL) {
+  switch (key) {
+    case 'app':
+      return configFile[key];
+    default:
+      return configFile;
+  }
+}
 
 function router() {
   return Express.Router();
@@ -7,5 +17,6 @@ function router() {
 
 
 module.exports = express = {
+  config,
   router
 };
